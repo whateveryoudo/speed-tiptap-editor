8b9e567a8674044452a7baf26c2c53ae4fbdea89
ykx <15982819091@163.com>
2025-10-14 20:57:44 +0800

    feat:调整路由为hash模式

--- example/src/router/index.ts
+++ example/src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import { speedTiptapLogin } from "../api/user";
 import { useGlobalStore } from '#example/store/index';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
